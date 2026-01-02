8f9b28517a3066ba33f691b7fe1718e93a1cff09
Luiz Felipe <luizkirchbaner.dev@gmail.com>
2026-01-01 21:28:11 -0300

    doc: documentacao da rota de login

--- src/features/auth/auth.routes.ts
+++ src/features/auth/auth.routes.ts
@@ -4,6 +4,35 @@ import { AuthController } from './auth.controller'
 const authRoutes = Router()
 const authController = new AuthController()
 
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Authenticate user
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: user@email.com
+ *               password:
+ *                 type: string
+ *                 example: 123456
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *       400:
+ *         description: Invalid input
+ */
 authRoutes.post('/login', (request, response) =>
   authController.login(request, response)
 )
